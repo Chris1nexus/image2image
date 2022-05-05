0aa67ad2562ae3cf0a601d4ced711b281fcc8835
chris1nexus <chris.2nexus@gmail.com>
2022-05-05 13:23:58 +0200

    Modify setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,11 @@
 from setuptools import setup, find_packages
 
-with open("requirements.txt", "r") as f:
-    requirements = f.read().splitlines()
+with open('requirements.txt') as req_file:
+    requirements = [req.strip() for req in req_file.read().splitlines()]
 
-setup(name="im2im", install_requires=requirements, packages=find_packages(), include_package_data=True,
-      package_data={'im2im': ['models/card/*.md']},)
+setup(name="im2im", install_requires=requirements, 
+        dependency_links=['https://download.pytorch.org/whl/lts/1.8/torch_lts.html'],
+    packages=find_packages(), 
+    include_package_data=True,
+    package_data={'im2im': ['models/card/*.md']},)
+
